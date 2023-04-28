014812fb5cc1535193b9cb792e631ded0497ed60
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-04-28 13:32:31 +0100

    wip

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Nav from './Nav.svelte';
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
+</svelte:head>
+
+<header class="container">
+	<Nav />
+	<!-- <hgroup> -->
+	<!-- 	<h1></h1> -->
+	<!-- 	<h2>A starter example with all elements and components used in Pico design system.</h2> -->
+	<!-- </hgroup> -->
+</header>
+
+<main class="container">
+<slot />
+</main>
---
+++ src/routes/Nav.svelte
@@ -0,0 +1,10 @@
+<nav>
+	<ul>
+		<li><strong>Form Demo</strong></li>
+	</ul>
+	<ul>
+		<li><a href="/js">js</a></li>
+		<li><a href="/nojs">no-js</a></li>
+		<li><a href="/superforms">superforms</a></li>
+	</ul>
+</nav>
